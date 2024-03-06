fe11ed1555aca7deea5d5401198d557cfdcd4c08
Schwick3 <sky321949@gmail.com>
2024-03-05 18:52:41 -0600

    Added PizzaCookingFactory class

---
+++ src/edu/mu/pizza/PizzaCookingFactory.java
@@ -0,0 +1,8 @@
+package edu.mu.pizza;
+
+public class PizzaCookingFactory {
+	// # TODO: make this method after the pizza classes are done
+	public AbstractPizza createPizza(PizzaType pizzaType) {
+		
+	}
+}
