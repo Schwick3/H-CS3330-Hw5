d8ee039ee40549f818e8e4f129b99e3ad1f4b07a
Schwick3 <sky321949@gmail.com>
2024-03-05 18:49:01 -0600

    Added CookingStyleType, PizzaType, and Toppings Enums

---
+++ src/edu/mu/pizza/CookingStyleType.java
@@ -0,0 +1,8 @@
+package edu.mu.pizza;
+
+public enum CookingStyleType {
+
+	MICROWAVE,
+	CONVENTIONAL_OVEN,
+	BRICK_OVEN;
+}
---
+++ src/edu/mu/pizza/PizzaType.java
@@ -0,0 +1,9 @@
+package edu.mu.pizza;
+
+public enum PizzaType {
+
+	HAWAIIAN,
+	MARGHERITA,
+	SUPREME,
+	VEGETARIAN
+}
