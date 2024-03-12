b4fa9e1d2aa3365602d9d53dca3accdab0ef055d
Schwick3 <sky321949@gmail.com>
2024-03-12 15:14:40 -0500

    Added PizzaCookingFactory Class

--- src/edu/mu/pizza/PizzaCookingFactory.java
+++ src/edu/mu/pizza/PizzaCookingFactory.java
@@ -3,6 +3,27 @@ package edu.mu.pizza;
 public class PizzaCookingFactory {
 	// # TODO: make this method after the pizza classes are done
 	public AbstractPizza createPizza(PizzaType pizzaType) {
-		
+		switch(pizzaType) {
+		case HAWAIIAN:
+			HawaiianPizza pizza = new HawaiianPizza();
+			AbstractPizza.setOrderIDCounter(AbstractPizza.getOrderIDCounter() + 1);
+			pizza.setPizzaOrderID(AbstractPizza.getOrderIDCounter());
+		case MARGHERITA:
+			MargheritaPizza pizza = new MargheritaPizza();
+			AbstractPizza.setOrderIDCounter(AbstractPizza.getOrderIDCounter() + 1);
+			pizza.setPizzaOrderID(AbstractPizza.getOrderIDCounter());
+		case SUPREME:
+			SupremePizza pizza = new SupremePizza();
+			AbstractPizza.setOrderIDCounter(AbstractPizza.getOrderIDCounter() + 1);
+			pizza.setPizzaOrderID(AbstractPizza.getOrderIDCounter());
+		case VEGETARIAN:
+			VegetarianPizza pizza = new VegetarianPizza();
+			AbstractPizza.setOrderIDCounter(AbstractPizza.getOrderIDCounter() + 1);
+			pizza.setPizzaOrderID(AbstractPizza.getOrderIDCounter());
+			
+		default:
+				break;
+		}
+		return null;
 	}
 }
