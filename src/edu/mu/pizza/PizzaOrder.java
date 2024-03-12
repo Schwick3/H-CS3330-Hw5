129a93dcca5135a7c0108f0d03c4c281930296df
karsyn williams <karsyn.williams15@gmail.com>
2024-03-12 11:54:29 -0500

    added PizzaOrder class

---
+++ src/edu/mu/pizza/PizzaOrder.java
@@ -0,0 +1,11 @@
+package edu.mu.pizza;
+
+public class PizzaOrder {
+	
+	private PizzaCookingFactory pizzaFactory;
+	
+	private ICookingStrategy cookingstrategy;
+	
+	private List<AbstractPizza> pizzaOrderList;
+
+}
