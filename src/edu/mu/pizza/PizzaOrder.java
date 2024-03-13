e6236a79f6c8038de79f239c4da51eba9d407f8d
Kyle Blank <kcgblank@gmail.com>
2024-03-12 22:20:44 -0500

    Added constructors, printPizza, addPIzza, removeTop, and checkout

--- src/edu/mu/pizza/PizzaOrder.java
+++ src/edu/mu/pizza/PizzaOrder.java
@@ -1,6 +1,7 @@
 package edu.mu.pizza;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class PizzaOrder {
@@ -44,6 +45,82 @@ public class PizzaOrder {
 				
 			}
 		}
+	public PizzaOrder (PizzaCookingFactory pizzaFactory, List<AbstractPizza> pizzaOrderList) { // variable constructor
+		this.pizzaFactory = pizzaFactory;
+		this.pizzaOrderList = pizzaOrderList;
+	}
+	public PizzaOrder() { //default constructor
+		pizzaOrderList = new ArrayList<AbstractPizza>();
+		pizzaFactory = new PizzaCookingFactory();
+	}
+	public void printPizzaOrderCart (int orderID) {
+		
+		for (AbstractPizza pizza: pizzaOrderList) { //iterates through the pizza and prints each. 
+			System.out.println(pizza);
+		}
+	}
+	
+	public boolean addPizzaToCart (PizzaType pizzatype) {
+			
+	
+		try { // tries to create a pizza of the given type and add it to the pizza order
+			AbstractPizza pizza = pizzaFactory.createPizza(pizzatype);
+			
+			pizzaOrderList.add(pizza);
+			
+			return true;
+		}
+		catch (Exception e) { // catches the exception if thrown and returns failure.
+			e.printStackTrace();
+			return false;
+		}
+			
+	}
+	public boolean removeToppingFromPizza(int orderID, Toppings topping) {
+		//This for loop goes through looking for the correct pizzaID
+		for (AbstractPizza pizza: pizzaOrderList) {
+			if (pizza.getPizzaOrderID() == orderID) {
+				List<Toppings> toppings = pizza.getToppingList();
+				Iterator<Toppings> iterator = toppings.iterator();
+				//iterates through the toppings on the desired pizza.
+				while (iterator.hasNext()) {
+					Toppings top = iterator.next();
+					if (top == topping) { // if the right topping is found it is removed. 
+						iterator.remove();
+						pizza.updatePizzaPrice();
+						return true;
+					}
+					else { // else it returns false to and nothing is changed on the pizza.
+						return false;
+						
+						
+					}
+				}
+			}
+		}
+		
+		
+		
+		return false;
+		
+	}
+	
+	public double checkout() throws Exception{
+		double total = 0;
+		if (isThereAnyUncookedPizza()) { // checks for uncooked pizzas before checking out. 
+			throw new Exception("There are still pizzas to be cooked");
+		}
+		else {
+			
+			for (AbstractPizza pizza: pizzaOrderList) {
+				total += pizza.getTotalPrice();
+				// if the whole order is cooked, then the total is calculated and the price returned. 
+				
+			}
+		}
+		
+		return total;
+	}
 		
 
 
